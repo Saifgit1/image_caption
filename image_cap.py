9b5fbd96f474a9042704fde2d78de0bec67bedad
Saifgit1 <msullahshahid1@gmail.com>
2026-03-14 12:34:51 -0400

    inital commit

---
+++ image_cap.py
@@ -0,0 +1,19 @@
+from PIL import Image 
+import requests 
+from transformers import AutoProcessor, BlipForConditionalGeneration 
+
+
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+image_path = "Imagee.jpg"
+image = Image.open(image_path,).convert("RGB")
+text = "the image of"
+
+inputs = processor(images = image , text = text , return_tensors='pt')
+outputs = model.generate(**inputs, max_length = 50)
+
+caption = processor.decode(outputs[0], skip_special_tokens = True)
+print(caption)
+
+
